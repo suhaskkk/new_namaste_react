a985b8309c325007cee84d167d5f79bfe7cfbe9c
Suhas Kambale <suhas.kambale@neosoftmail.com>
2023-11-14 12:12:08 +0530

    new code

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,30 @@
 import FoodCard from "./FoodCard";
 import { resData } from "../utils/restData";
+import { useState } from "react";
 
 const Body = () => {
-    return (
+  const [restoData, setRestoData] = useState(resData);
+  return (
+    <div>
       <div>
-        <div>
-          <h3>search</h3>
-        </div>
-        <div className="food_card_container">
-          {resData.map((restaurant) => (
-            <FoodCard key={restaurant.id} resCard={restaurant} />
-          ))}
-        </div>
+        <button
+          onClick={() => {
+            let filterdata = restoData.filter((el) => {
+              return el.stars > 4;
+            });
+            setRestoData(filterdata);
+          }}
+        >
+          highest ratings
+        </button>
       </div>
-    );
-  };
+      <div className="food_card_container">
+        {restoData.map((restaurant) => (
+          <FoodCard key={restaurant.id} resCard={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-  export default Body;
+export default Body;
--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -1,11 +1,9 @@
-
-
 const FoodCard = (props) => {
   const { resCard } = props;
   const { resName, cuisine, stars, time, img } = resCard;
   return (
     <div className="card_size">
-      <img className="card_logo" src={img} alt="" />
+      <img className="card_logo" src={img} alt="  " />
       <h3>{resName}</h3>
       <h4>{cuisine}</h4>
       <h4>{stars}</h4>
