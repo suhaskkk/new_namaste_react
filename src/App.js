25fe09bdda87ea86b2ab8efed8e2722579e8b52f
Suhas Kambale <suhas.kambale@neosoftmail.com>
2023-11-26 13:22:58 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -2,30 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { BrowserRouter ,RouterProvider } from "react-router-dom";
 import About from "./components/About";
-
+import Error from "./components/Error";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
-    </div> 
+      <Outlet />
+    </div>
   );
 };
 
-const appRouter = BrowserRouter([
+const appRouter = createBrowserRouter([
   {
-   path: "/",
-   element: <AppLayout />
-},
-{
-  path: "/about",
-  element: <About />
-}
-])
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<RouterProvider router={appRouter}  />);
+root.render(<RouterProvider router={appRouter} />);
---
+++ src/components/Error.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+  const err = useRouteError();
+  console.log(err);
+  return <div>Error </div>;
+};
+
+export default Error;
